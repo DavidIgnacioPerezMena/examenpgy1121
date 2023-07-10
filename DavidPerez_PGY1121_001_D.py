3fb13b3700bf7ece92ab9b9d3052a20856a9269b
DavidIgnacioPerezMena <139148058+DavidIgnacioPerezMena@users.noreply.github.com>
2023-07-10 11:16:25 -0400

    Examen Transversal

---
+++ DavidPerez_PGY1121_001_D.py
@@ -0,0 +1,109 @@
+asientos=[14,"no disponible", 15,"no disponible",16,"no disponible",33,"no disponible",45,"no disponible"]
+asientoss=[1,2,3,4,5,6,7,8,9,10,11,12,3,17,18,9,20,21,22,23,24,25,26,27,28,29,30,31,32,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
+
+def 1:
+print("""
+            Platinum:120.000 (del 1 al 20)
+            Gold    :80.000  (del 21 al 50)
+            Silver  :50.000  (del 51 al 100)
+            """)
+print=input("Ingrese el asiento que desee")
+match 
+if 1:
+    input=14 
+    print(asientos)
+elif 2:
+    input=45 
+    print(asientos)
+elif 3:
+    input=33 
+    print(asientos)
+elif 4:
+        input=16 
+        print(asientos)
+elif 5:
+    input=15 
+    print(asientos)
+else:
+    for asiento in asientoss:
+        print=input("Ingrese su run (Es solo 1 run por asiento)")
+        run=input
+        run > 55555555 and  run < 99999999
+        print(f"{run} registrado")
+        case 1:
+        total= asiento +120.000
+        tot= asiento + 80.000
+        totall= asiento + 50.000
+        asiento is 1,2,3,4,5,6,7,8,9,10,11,12,3,17,18,9,20
+        asiento + 120.000
+        case 2:
+        asiento is 21,22,23,24,25,26,27,28,29,30,31,32,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50
+        asiento + 80.000
+        case 3:
+        asiento is 51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100
+        asiento + 50.000
+        case other:
+        
+
+def 2 :
+print("""
+                Escenario
+            1 2 3 4 5 6 7 8 9 10 
+        11 12 13 x x x 17 18 19 20
+        21 22 23 24 25 26 27 28 29 30
+        31 32 x 34 35 36 37 38 39 40
+        41 42 43 44 x 46 47 48 49 50
+        51 52 53 54 55 56 57 58 59 60
+        61 62 63 64 65 66 67 68 69 70
+        71 72 73 74 75 76 77 78 79 80
+        81 82 83 84 85 86 87 88 89 90
+        91 92 93 94 95 96 97 98 99 100
+
+
+        """)#vista sin agregar ningun asiento
+
+
+def 3:
+print(f"""Asistentes ya confirmados al concierto
+      -------------------
+      051732xx
+      073471xx
+      128632xx
+      185830xx
+      186824xx
+      {run}
+      -------------------
+      """)
+
+
+def 4:
+print(f"""
+Tipo entrada                 Cantidad entrada           Total      
+Platinum $120.000                 {asiento}              ${total}
+Gold     $80.000                  {asiento}              ${tot}
+Silver   $50.000                  {asiento}              ${totall}
+Total                          {asiento+asiento+asiento} ${total+tot+totall}
+En caso de querer otro asiento ingrese nuevamente para registrar a otra persona
+""")#vista previa de como deberia ser la boleta final
+
+
+def 5:
+print("""Saliendo del sistema...
+David Ignacio Pérez Mena
+10/07/2023
+""")
+
+
+while True:
+        print(""" 'Creativos.cl'
+        Bienvenido a la compra de entradas para concierto de Michael Jam""")
+        print=(float(input("""
+        1) Compra de entradas
+        2)Mostrar ubicaciones disponibles
+        3)ver listado de asistentes
+        4)Mostrar ganancias totales
+        5)Salir
+        """)))
+    
+
+#https://github.com/DavidIgnacioPerezMena/examenpgy1121
